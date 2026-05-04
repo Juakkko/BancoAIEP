1eb1630d4f017da9647b79847fb87e3f1d7b75bc
JorgeFlowers12 <jorge.efi123@gmail.com>
2026-05-04 19:43:25 -0400

    cambios en el login

--- Login.php
+++ Login.php
@@ -1,20 +1,44 @@
 <?php
-include 'db.php';
+
 session_start();
-$error = "";
+include 'db.php';
 
-if ($_POST) {
-    $stmt = mysqli_prepare($conexion, "SELECT * FROM Cliente WHERE rut = ? AND activo = 1");
-    mysqli_stmt_bind_param($stmt, "s", $_POST['rut']);
-    mysqli_stmt_execute($stmt);
-    $user = mysqli_fetch_assoc(mysqli_stmt_get_result($stmt));
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $rut = trim($_POST['rut']);
+    $pass = $_POST['password'];
 
-    if ($user && $user['password'] === $_POST['password']) {
-        $_SESSION['user_id'] = $user['id_cliente'];
-        $_SESSION['user_name'] = $user['nombre'] . " " . $user['apellido'];
+    $sql = "SELECT id_cliente, nombre, apellido, password FROM Cliente WHERE rut = ? AND activo = 1";
+
+    if ($stmt = mysqli_prepare($conexion, $sql)) {
+        mysqli_stmt_bind_param($stmt, "s", $rut);
+        mysqli_stmt_execute($stmt);
+        $resultado = mysqli_stmt_get_result($stmt);
+
+        if ($user = mysqli_fetch_assoc($resultado)) {
+
+            if ($user['password'] === $pass) { 
+                $_SESSION['user_id'] = $user['id_cliente'];
+                $_SESSION['user_name'] = $user['nombre'] . " " . $user['apellido'];
+
+                header("Location: dashboard.php");
+                exit();
+            } else {
+
+                $_SESSION['error_login'] = "La contraseña ingresada es incorrecta.";
+                header("Location: Index.php");
+                exit();
+            }
+        } else {
+
+            $_SESSION['error_login'] = "El RUT no existe o la cuenta está inactiva.";
+            header("Location: Index.php");
+            exit();
+        }
+        mysqli_stmt_close($stmt);
+    } else {
+        $_SESSION['error_login'] = "Error interno del servidor. Intente más tarde.";
         header("Location: Index.php");
-        exit;
+        exit();
     }
-
 }
 ?>
